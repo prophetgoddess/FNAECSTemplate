bce1b2e85b9a9c902da92a5ef160d03e6ec9e5ce
Cassandra Lugo <prophet_goddess@protonmail.com>
2023-01-08 20:52:46 -0800

    template files

---
+++ FNAECSTemplate/Game1.cs
@@ -0,0 +1,49 @@
+using System;
+
+using Microsoft.Xna.Framework;
+
+namespace FNATemplate
+{
+    public class Game1 : Game
+    {
+        [STAThread]
+        internal static void Main()
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+        private Game1()
+        {
+            GraphicsDeviceManager gdm = new GraphicsDeviceManager(this);
+
+            gdm.PreferredBackBufferWidth = 1024;
+            gdm.PreferredBackBufferHeight = 768;
+            gdm.SynchronizeWithVerticalRetrace = true;
+
+            IsFixedTimeStep = false;
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
