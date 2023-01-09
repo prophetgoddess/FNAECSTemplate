bf477f4173ee6d8fc5c135c55564e460358705e6
Cassandra Lugo <prophet_goddess@protonmail.com>
2023-01-08 23:08:51 -0800

    cornflower blue

--- FNAECSTemplate/Game1.cs
+++ FNAECSTemplate/Game1.cs
@@ -43,6 +43,7 @@ namespace FNAECSTemplate
 
         protected override void Draw(GameTime gameTime)
         {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
             base.Draw(gameTime);
         }
     }
