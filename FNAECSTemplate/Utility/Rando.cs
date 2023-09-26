8f30fc815530ad99edc44f453686b6f381cbd5f7
cass <prophet_goddess@protonmail.com>
2023-09-26 12:41:28 -0700

    namespace whoops

--- FNAECSTemplate/Utility/Rando.cs
+++ FNAECSTemplate/Utility/Rando.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 
-namespace ShmupTutorial.Utility;
+namespace FNAECSTemplate.Utility;
 public static class Rando
 {
     static Random Rand { get; } = new Random();
