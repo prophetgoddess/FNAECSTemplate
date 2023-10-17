98a9bdcbb9233f57ca3250c23255cd3b9e1945e1
cass <prophet_goddess@protonmail.com>
2023-10-17 16:43:50 -0700

    file scoped namespaces

--- FNAECSTemplate/Game1.cs
+++ FNAECSTemplate/Game1.cs
@@ -9,129 +9,128 @@ using FNAECSTemplate.Renderers;
 using FontStashSharp;
 using FNAECSTemplate.Content;
 
-namespace FNAECSTemplate
+namespace FNAECSTemplate;
+
+public class Game1 : Game
 {
-    public class Game1 : Game
+    GraphicsDeviceManager GraphicsDeviceManager { get; }
+
+    /*
+    the World is the place where all our entities go.
+    */
+    World World { get; } = new World();
+
+    Input Input;
+    ExampleSystem ExampleSystem;
+    ExampleRenderer ExampleRenderer;
+
+    SpriteBatch SpriteBatch;
+
+    [STAThread]
+    internal static void Main()
     {
-        GraphicsDeviceManager GraphicsDeviceManager { get; }
+        using (Game1 game = new Game1())
+        {
+            game.Run();
+        }
+    }
+    private Game1()
+    {
+        //setup our graphics device, default window size, etc
+        //here is where i will make a plea to you, intrepid game developer:
+        //please default your game to windowed mode.
+        GraphicsDeviceManager = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+
+        GraphicsDeviceManager.PreferredBackBufferWidth = 1024;
+        GraphicsDeviceManager.PreferredBackBufferHeight = 768;
+        GraphicsDeviceManager.SynchronizeWithVerticalRetrace = true;
+
+        IsFixedTimeStep = false;
+        IsMouseVisible = true;
+    }
+
+    //you'll want to do most setup in LoadContent() rather than your constructor.
+    protected override void LoadContent()
+    {
+        /*
+        CONTENT
+        */
 
         /*
-        the World is the place where all our entities go.
+        SpriteBatch is FNA/XNA's abstraction for drawing sprites on the screen.
+        you want to do is send all the sprites to the GPU at once, 
+        it's much more efficient to send one huge batch than to send sprites piecemeal. 
+        See more in the Renderers/ExampleRenderer.cs. 
         */
-        World World { get; } = new World();
+        SpriteBatch = new(GraphicsDevice);
 
-        Input Input;
-        ExampleSystem ExampleSystem;
-        ExampleRenderer ExampleRenderer;
+        AllContent.Initialize(Content);
+        /*
+        SYSTEMS
+        */
 
-        SpriteBatch SpriteBatch;
+        /*
+        here we set up all our systems. 
+        you can pass in information that these systems might need to their constructors.
+        it doesn't matter what order you create the systems in, we'll specify in what order they run later.
+        */
+        ExampleSystem = new(World);
+        Input = new(World);
 
-        [STAThread]
-        internal static void Main()
+        /*
+        RENDERERS
+        */
+
+        //same as above, but for the renderer
+        ExampleRenderer = new(World, SpriteBatch);
+
+        /*
+        ENTITIES
+        */
+
+        for (int i = 0; i < 10; i++)
         {
-            using (Game1 game = new Game1())
-            {
-                game.Run();
-            }
-        }
-        private Game1()
-        {
-            //setup our graphics device, default window size, etc
-            //here is where i will make a plea to you, intrepid game developer:
-            //please default your game to windowed mode.
-            GraphicsDeviceManager = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-
-            GraphicsDeviceManager.PreferredBackBufferWidth = 1024;
-            GraphicsDeviceManager.PreferredBackBufferHeight = 768;
-            GraphicsDeviceManager.SynchronizeWithVerticalRetrace = true;
-
-            IsFixedTimeStep = false;
-            IsMouseVisible = true;
-        }
-
-        //you'll want to do most setup in LoadContent() rather than your constructor.
-        protected override void LoadContent()
-        {
-            /*
-            CONTENT
-            */
-
-            /*
-            SpriteBatch is FNA/XNA's abstraction for drawing sprites on the screen.
-            you want to do is send all the sprites to the GPU at once, 
-            it's much more efficient to send one huge batch than to send sprites piecemeal. 
-            See more in the Renderers/ExampleRenderer.cs. 
-            */
-            SpriteBatch = new(GraphicsDevice);
-
-            AllContent.Initialize(Content);
-            /*
-            SYSTEMS
-            */
-
-            /*
-            here we set up all our systems. 
-            you can pass in information that these systems might need to their constructors.
-            it doesn't matter what order you create the systems in, we'll specify in what order they run later.
-            */
-            ExampleSystem = new(World);
-            Input = new(World);
-
-            /*
-            RENDERERS
-            */
-
-            //same as above, but for the renderer
-            ExampleRenderer = new(World, SpriteBatch);
-
-            /*
-            ENTITIES
-            */
-
-            for (int i = 0; i < 10; i++)
-            {
-                var e = World.CreateEntity();
-                World.Set(e, new ExampleComponent(0f));
-            }
-
-            base.LoadContent();
+            var e = World.CreateEntity();
+            World.Set(e, new ExampleComponent(0f));
         }
 
-        //sometimes content needs to be unloaded, but it usually doesn't.
-        protected override void UnloadContent()
-        {
-            base.UnloadContent();
-        }
+        base.LoadContent();
+    }
+
+    //sometimes content needs to be unloaded, but it usually doesn't.
+    protected override void UnloadContent()
+    {
+        base.UnloadContent();
+    }
 
 
-        protected override void Update(GameTime gameTime)
-        {
-            /*
-            here we call all our system update functions. 
-            call them in the order you want them to run. 
-            other ECS libraries have a master "update" function that does this for you,
-            but moontools.ecs does not. this lets you have more control
-            over the order systems run in, and whether they run at all.
-            */
-            Input.Update(gameTime.ElapsedGameTime); //always update this before anything that takes inputs
-            ExampleSystem.Update(gameTime.ElapsedGameTime);
-            World.FinishUpdate(); //always call this at the end of your update function.
-            base.Update(gameTime);
-        }
+    protected override void Update(GameTime gameTime)
+    {
+        /*
+        here we call all our system update functions. 
+        call them in the order you want them to run. 
+        other ECS libraries have a master "update" function that does this for you,
+        but moontools.ecs does not. this lets you have more control
+        over the order systems run in, and whether they run at all.
+        */
+        Input.Update(gameTime.ElapsedGameTime); //always update this before anything that takes inputs
+        ExampleSystem.Update(gameTime.ElapsedGameTime);
+        World.FinishUpdate(); //always call this at the end of your update function.
+        base.Update(gameTime);
+    }
 
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue); //set the color of the background. cornflower blue is XNA tradition.
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue); //set the color of the background. cornflower blue is XNA tradition.
 
-            /*
-            call renderers here.
-            renderers don't get passed the game time. 
-            if you are thinking about passing the game time to a renderer
-            in order to do something, try doing it some other way. you'll thank me later.
-            */
-            ExampleRenderer.Draw();
-            base.Draw(gameTime);
-        }
+        /*
+        call renderers here.
+        renderers don't get passed the game time. 
+        if you are thinking about passing the game time to a renderer
+        in order to do something, try doing it some other way. you'll thank me later.
+        */
+        ExampleRenderer.Draw();
+        base.Draw(gameTime);
     }
 }
--- FNAECSTemplate/Systems/ExampleSystem.cs
+++ FNAECSTemplate/Systems/ExampleSystem.cs
@@ -4,44 +4,43 @@ using FNAECSTemplate.Components;
 using FNAECSTemplate.Messages;
 
 
-namespace FNAECSTemplate.Systems
-{
-    /*
-    this is a system. systems can read and modify components and entities. 
-    */
-    public class ExampleSystem : MoonTools.ECS.System
-    {
-        private Filter ExampleFilter { get; }
-        private Random rnd { get; } = new Random();
+namespace FNAECSTemplate.Systems;
 
-        public ExampleSystem(World world) : base(world)
+/*
+this is a system. systems can read and modify components and entities. 
+*/
+public class ExampleSystem : MoonTools.ECS.System
+{
+    private Filter ExampleFilter { get; }
+    private Random rnd { get; } = new Random();
+
+    public ExampleSystem(World world) : base(world)
+    {
+        /*
+        this is a filter. a filter allows us to specify which entities we do and don't want.
+        a filter will, every frame, get us every single entity that matches its criteria.
+        */
+        ExampleFilter = FilterBuilder
+                        .Include<ExampleComponent>()
+                        .Build();
+    }
+
+    public override void Update(TimeSpan delta)
+    {
+        //we get the entities that the filter has found this frame from ExampleFilter.Entities.
+        //the filter is programmed in such a way that it can very efficiently query every single
+        //entity in the game and retrieve only the ones we want, even if there are thousands of entities.
+        foreach (var example in ExampleFilter.Entities)
         {
             /*
-            this is a filter. a filter allows us to specify which entities we do and don't want.
-            a filter will, every frame, get us every single entity that matches its criteria.
+            then, we use Set to set the component on the entity.
+            if the property ExampleProperty on ExampleComponent weren't readonly
+            and we just changed it to some other value, it wouldn't actually change the value
+            in the component store. we have to call Set() to update the component store so that
+            other systems can read our changes and they'll persist across frames.
             */
-            ExampleFilter = FilterBuilder
-                            .Include<ExampleComponent>()
-                            .Build();
-        }
-
-        public override void Update(TimeSpan delta)
-        {
-            //we get the entities that the filter has found this frame from ExampleFilter.Entities.
-            //the filter is programmed in such a way that it can very efficiently query every single
-            //entity in the game and retrieve only the ones we want, even if there are thousands of entities.
-            foreach (var example in ExampleFilter.Entities)
-            {
-                /*
-                then, we use Set to set the component on the entity.
-                if the property ExampleProperty on ExampleComponent weren't readonly
-                and we just changed it to some other value, it wouldn't actually change the value
-                in the component store. we have to call Set() to update the component store so that
-                other systems can read our changes and they'll persist across frames.
-                */
-                foreach (var input in ReadMessages<InputAction>())
-                    Set(example, new ExampleComponent(rnd.NextSingle()));
-            }
+            foreach (var input in ReadMessages<InputAction>())
+                Set(example, new ExampleComponent(rnd.NextSingle()));
         }
     }
 }
